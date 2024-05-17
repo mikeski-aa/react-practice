4c841a7eb8cda037e2e1eefb4375e9085d05b304
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-05-17 11:40:57 +0200

    practicing with states

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,51 @@
 import React, { useState } from "react";
 import "./App.css";
 
-const COLORS = ["pink", "green", "blue", "yellow", "purple"];
-let counter = 0;
+function InputField({typeOfInput, text, changeFunc}) {
+
+  
+  return (
+    <>
+      <label htmlFor="name">{typeOfInput}
+      <input type="text" id="name" value={text} onChange={changeFunc}></input>
+      </label>
+    </>
+  )
+}
+
 
 function App() {
-  const [backgroundColor, setBackgroundColor] = useState(COLORS[0]);
+  const [person, setPerson] = useState({ name: "John", hobby: "coding", age: 100});
+  const [text, setText] = useState("");
 
-  const onButtonClick = (color) => () => {
-    setBackgroundColor(color);
-    counter += 1;
-  };
+  const handleIncreaseAge = () => {
+    const newPerson = { ...person, age: person.age + 1 };
+    setPerson(newPerson);
+    
+  }
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  }
+
+
 
   return (
-    <div
-      className="App"
-      style={{
-        backgroundColor,
-      }}
-    >
-      <div className="counter">Current count: {counter}</div>
-      {COLORS.map((color) => (
-        <button
-          type="button"
-          key={color}
-          onClick={onButtonClick(color)}
-          className={backgroundColor === color ? "selected" : ""}
-        >
-          {color}
-        </button>
-      ))}
-    </div>
-  );
+    <>
+      <InputField 
+      typeOfInput="Name" 
+      text={text}
+      changeFunc={handleChange}/>
+      <InputField 
+      typeOfInput="Surname"
+      text={text}
+      changeFunc={handleChange}/>
+      <h1>{person.name}</h1> 
+      <h1>{person.hobby}</h1>
+      <h1>{person.age}</h1>
+      <button onClick={handleIncreaseAge}>Increase age button</button> 
+    </>
+  )
 }
 
 export default App;
